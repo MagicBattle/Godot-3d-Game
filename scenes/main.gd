634f09aef3ff907659e08e41d7819c8ad535a916
MagicBattle <126199569+MagicBattle@users.noreply.github.com>
2025-09-29 22:14:34 -0700

    Added UI system and music to the game

--- scenes/main.gd
+++ scenes/main.gd
@@ -2,6 +2,9 @@ extends Node
 
 @export var mob_scene: PackedScene
 
+func _ready():
+	$UserInterface/Retry.hide() 
+
 func _on_mob_timer_timeout():
 	# Create a new instance of the Mob scene.
 	var mob = mob_scene.instantiate()
@@ -16,8 +19,17 @@ func _on_mob_timer_timeout():
 	mob.initialize(mob_spawn_location.position, player_position)
 
 	# Spawn the mob by adding it to the Main scene.
-	add_child(mob)
+	add_child(mob) 
+	# We connect the mob to the score label to update the score upon squashing one.
+	mob.squashed.connect($UserInterface/ScoreLabel._on_mob_squashed.bind())
 
 
 func _on_player_hit() -> void:
-	$MobTimer.stop()
+	$MobTimer.stop() 
+	$UserInterface/Retry.show()
+	
+	
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_accept") and $UserInterface/Retry.visible:
+		# This restarts the current scene.
+		get_tree().reload_current_scene()
